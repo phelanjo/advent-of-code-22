e63d956dc56f6a289e5a00359fba562f0cee48c1
Johnny Phelan <johnny@getcairn.com>
2022-12-07 13:24:40 -0700

    Day 1 Part 2 update

--- day-1/calorie_counting.py
+++ day-1/calorie_counting.py
@@ -1,4 +1,3 @@
-# Part 1
 def get_caloric_amounts_list_from_input(filename):
     with open(filename, 'r') as input_file:
         lines = input_file.readlines()
@@ -8,21 +7,33 @@ def get_caloric_amounts_list_from_input(filename):
 
 def elf_calorie_counter(input_file):
     caloric_amounts = get_caloric_amounts_list_from_input(input_file)
+    elf_caloric_sums_list = []
     current_elf_sum = 0
-    top_elf_sum = 0
 
     for caloric_amount in caloric_amounts:
         if caloric_amount is not '':
             current_elf_sum += int(caloric_amount)
         else:
-            if current_elf_sum > top_elf_sum:
-                top_elf_sum = current_elf_sum
-                current_elf_sum = 0
-            else:
-                current_elf_sum = 0
+            elf_caloric_sums_list.append(current_elf_sum)
+            current_elf_sum = 0
     
-    return top_elf_sum
+    elf_caloric_sums_list.append(current_elf_sum)
+    
+    return elf_caloric_sums_list
 
-top_elf_calories = elf_calorie_counter(input_file="input.txt")
-print(top_elf_calories)
+def top_elf_caloric_value(elf_caloric_sums):
+    elf_caloric_sums.sort(reverse=True)
+    return elf_caloric_sums[0]
 
+def top_three_elves_caloric_values(elf_caloric_sums):
+    elf_caloric_sums.sort(reverse=True)
+    return elf_caloric_sums[0] + elf_caloric_sums[1] + elf_caloric_sums[2]
+
+# Output
+elf_caloric_sums_list = elf_calorie_counter("input.txt")
+
+top_elf = top_elf_caloric_value(elf_caloric_sums_list)
+print(top_elf)
+
+top_three_elves = top_three_elves_caloric_values(elf_caloric_sums_list)
+print(top_three_elves)
