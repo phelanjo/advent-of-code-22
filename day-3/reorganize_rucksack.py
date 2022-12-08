12adc71897b49e3f44120f8e08a3d520ba0ee095
Johnny Phelan <johnny@getcairn.com>
2022-12-08 10:06:50 -0700

    Day 3 - Part 2 Solution

--- day-3/reorganize_rucksack.py
+++ day-3/reorganize_rucksack.py
@@ -9,16 +9,34 @@ def letter_to_priority_number(letter):
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     return next((i+1 for i, _letter in enumerate(alphabet) if _letter == letter), None)
 
-def get_rucksack_priorities_sum(inputs):
+def get_rucksack_priorities_sum(input):
     rucksack_priorities_sum = 0
-    for input in inputs:
-        item_1, item_2 = input[:len(input)//2], input[len(input)//2:]
+    for i in input:
+        item_1, item_2 = i[:len(i)//2], i[len(i)//2:]
         common_item = ''.join(set(item_1).intersection(item_2))
         rucksack_priorities_sum += letter_to_priority_number(common_item)
     
     return rucksack_priorities_sum
 
-inputs = read_input('input.txt')
-rucksack_priorities_sum = get_rucksack_priorities_sum(inputs)
+input = read_input('input.txt')
+rucksack_priorities_sum = get_rucksack_priorities_sum(input)
 
 print(rucksack_priorities_sum)
+
+# Part 2
+def divide_chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def get_badge_priorities_sum(input):
+    badge_priorities_sum = 0
+    for badge_group in divide_chunks(input, 3):
+        elf_1, elf_2, elf_3 = badge_group
+        badge_letter = ''.join(set(elf_1).intersection(elf_2).intersection(elf_3))
+        badge_priorities_sum += letter_to_priority_number(badge_letter)
+
+    return badge_priorities_sum
+
+badge_priorities_sum = get_badge_priorities_sum(input)
+
+print(badge_priorities_sum)
